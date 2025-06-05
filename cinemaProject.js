9f9aab27512c70e1440934e393112e8ed5d239bf
Shaunak Pandya <176204036+SRPpandya2004@users.noreply.github.com>
2025-06-05 23:20:39 +0530

    Update cinemaProject.js

--- cinemaProject.js
+++ cinemaProject.js
@@ -1,7 +1,6 @@
 const hamburgerMenuElement = document.getElementsByClassName("hamburger-menu")[0];
 const navDivElement = document.getElementsByClassName("navigation-links-div")[0];
 
-
 hamburgerMenuElement.addEventListener('click', (evt) => {
     evt.preventDefault();
     navDivElement.classList.toggle('active');
